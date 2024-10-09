179182ab5315dd294d540aa99f284faa6abc0718
adaa7 <1006253277@qq.com>
2024-10-09 15:21:28 +0800

    Feat: 添加读取外部JSON配置文件

--- common/src/main/java/com/adaa7/common/utils/FileUtil.java
+++ common/src/main/java/com/adaa7/common/utils/FileUtil.java
@@ -7,14 +7,13 @@ import java.io.IOException;
 import java.util.UUID;
 
 public class FileUtil {
-    public static String UPLOADS_PATH= System.getProperty("user.dir") + "/File/";
     public static String uploadsFile(MultipartFile file) throws IOException {
         String fileSuffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf('.')+1);
         String fileName = UUID.randomUUID().toString().replace("-","")+"."+fileSuffix;
-        fileDir(UPLOADS_PATH);
-        File descFile = new File(UPLOADS_PATH,fileName);
+        fileDir(JsonConfigUtil.UPLOADS_PATH);
+        File descFile = new File(JsonConfigUtil.UPLOADS_PATH,fileName);
         file.transferTo(descFile);
-        return UPLOADS_PATH+fileName;
+        return JsonConfigUtil.UPLOADS_PATH+fileName;
     }
     private static void fileDir(String url) {
         File file = new File(url);
